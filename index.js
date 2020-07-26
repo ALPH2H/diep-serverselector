e80160b84f3702fadaa32f3481c46f5ccddde134
Cazka <can_oe@me.com>
2020-07-26 02:07:30 +0200

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT ?? 3000;
+
+app.use('/', (req, res) => {
+    res.sendFile(__dirname + '/dieptool-ServerSelector.png');
+});
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+});
